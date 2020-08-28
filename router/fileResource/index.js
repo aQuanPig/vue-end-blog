d317d7f8bccba0f46004cf11703867e7674d1123
小怂 <5270546+warm_indignation@user.noreply.gitee.com>
2020-08-28 10:04:01 +0800

    图片传到阿里云

--- router/fileResource/index.js
+++ router/fileResource/index.js
@@ -1,17 +1,17 @@
 //处理文件资源
 const multer = require('multer')
-// const MAO = require('multer-aliyun-oss');
+const MAO = require('multer-aliyun-oss');
 module.exports = (app, path, person) => {
   const upload = multer({
-    dest:path
-    // storage: MAO({
-    //   config: {
-    //     region: '<region>',
-    //     accessKeyId: '<accessKeyId>',
-    //     accessKeySecret: '<accessKeySecret>',
-    //     bucket: '<bucket>'
-    //   }
-    // })
+    // dest:path
+    storage: MAO({
+      config: {
+        region: 'oss-cn-shenzhen',
+        accessKeyId: 'LTAI4G8XiQwhDckLpyM9yx9Y',
+        accessKeySecret: 't6br32uJcwe1PUSx3KOCi6BxZ8guV5',
+        bucket: 'zz-vue-blog'
+      }
+    })
   })
   return app.post(`/${person}/api/uploads`, upload.single('file'), (req, res, next) => {
     // req.file 是 `file` 文件的信息
@@ -24,7 +24,7 @@ module.exports = (app, path, person) => {
     }
     // 修改URL，改成线上的
     // file.url = `http://localhost:3000/${fileName}/${file.filename}`
-    file.url = `http://47.107.149.243/${fileName}/${file.filename}`
+    // file.url = `http://47.107.149.243/${fileName}/${file.filename}`
     res.send(file)
   })
 }
